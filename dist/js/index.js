6d22fd9b197ba8b5fd31fb1a937b6269ed642ea7
Dika Wicaksono <dhkwck00@gmail.com>
2024-01-21 16:40:35 +0700

    add function to make navbar fixed

--- dist/js/index.js
+++ dist/js/index.js
@@ -6,3 +6,15 @@ hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("hamburger-active");
   navMenu.classList.toggle("hidden");
 });
+
+// Navbar Fixed
+window.onscroll = function () {
+  header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
+
+  if (window.scrollY > fixedNav) {
+    header.classList.add("navbar-fixed");
+  } else {
+    header.classList.remove("navbar-fixed");
+  }
+};
